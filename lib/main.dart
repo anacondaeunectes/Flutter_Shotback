c715168a291b7fe44469fd5ea84792df514c7bbb
Pablo <anaconda_eunectes@hotmail.com>
2020-10-09 23:29:02 +0200

    Update Descripcion y cambio color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+final String ejemplo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacus erat, scelerisque sit amet aliquet in, varius vitae diam. Vestibulum tempus ultrices justo, et scelerisque lectus tincidunt non. Aenean sit amet tortor et elit molestie rhoncus in eu purus. Etiam non sagittis lacus. Pellentesque hendrerit, risus ac volutpat cursus, lacus velit malesuada enim, eget ullamcorper velit elit consequat ante. Proin facilisis auctor augue eu bibendum. Quisque sed bibendum ipsum, ut commodo nunc. Pellentesque aliquet nisl nec diam ornare, in lobortis velit molestie. In ultricies purus non libero efficitur, id tempor magna maximus.";
+
 void main() {
   runApp(MainScreen());
 }
@@ -11,14 +13,10 @@ class MainScreen extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
+        backgroundColor: Colors.lime,
         appBar: AppBar(
           title: Text("Galería de imágenes"),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_left),
-            onPressed: () {
-              print("hola");
-            },
-          ),
+          backgroundColor: Colors.green
         ),
         body: ListView(
           children: <Widget>[
@@ -30,7 +28,7 @@ class MainScreen extends StatelessWidget {
                 "https://aws.traveler.es/prod/designs/v1/assets/1000x667/21250.jpg"),
                 "Fuji")),
             ShotbackCard(Shotback(Image.network(
-                "https://aws.traveler.es/prod/designs/v1/assets/1000x667/21250.jpg"), "Monte Fuji", Text("Viaje en el verano de 2003 a Japon"),DateTime.now(),"Japan" ))
+                "https://aws.traveler.es/prod/designs/v1/assets/1000x667/21250.jpg"), "Monte Fuji", ejemplo, DateTime.now(),"Japan" ))
           ],
         ),
       ),
@@ -52,39 +50,42 @@ ShotbackScreen(Shotback shotback) {
     return Scaffold(//Nuevo Scaffold para esta pantalla secundaria
       appBar: AppBar(//Este AppBar incluye por defecto un leadign Button que nos permite "poppear" esta pantalla y volve a la anterior
         title: Text(this.shotback.title),
+        backgroundColor: Colors.green
       ),
-      body: Column(//Cuerpo de la pantalla
+      body: ListView(//Cuerpo de la pantalla
         children: [
           Image.network("https://aws.traveler.es/prod/designs/v1/assets/1000x667/21250.jpg"),
           Card(//Card para la localizacion
-            color: Colors.lightBlueAccent,
+            color: Colors.green,
             child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: Row(
                 children: [
-                  Icon(Icons.place, color: Colors.white,),
-                  Text(this.shotback.location)
+                  Icon(Icons.place, color: Colors.black),
+                  VerticalDivider(color: Colors.white, thickness: 5, width: 5,),
+                  Text(this.shotback.location, style: TextStyle(color: Colors.white))
                 ],
               ),
             ),
           ),
           Card(//Card para la fecha
-            color: Colors.lightBlueAccent,
+            color: Colors.green,
             child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: Row(
                 children: [
-                  Icon(Icons.date_range, color: Colors.white),
-                  Text(this.shotback.date.toString())
+                  Icon(Icons.date_range, color: Colors.black),
+                  VerticalDivider(color: Colors.white, thickness: 5, width: 5,),
+                  Text(this.shotback.date.toString(), style: TextStyle(color: Colors.white))
                 ],
               ),
             ),
           ),
           Card(
-            color: Colors.lightBlueAccent,
+            color: Colors.green,
             child: Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Expanded(child: Row(children: [this.shotback.description],))
+              child: Column(children: [Text(this.shotback.description, style: TextStyle(color: Colors.white))])
             ),
           )
         ],
@@ -141,11 +142,12 @@ class ShotbackCard extends StatelessWidget {
 class Shotback {
   Image image;
   String title;
-  Text description;
+  String description;
   String location;
   DateTime date;
   //Contactos[]
 
   Shotback(this.image,//Parametro obligatorio
       [this.title = "", this.description, this.date, this.location]);//Parametros opcionales. El titulo por defecto es un string sin texto
+  
 }
